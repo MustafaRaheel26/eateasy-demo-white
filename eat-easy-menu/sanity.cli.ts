55c8fb56bb63fc5db1bc6ac84d9855167ef69776
MustafaRaheel26 <mustafaraheel26@gmail.com>
2026-03-08 02:19:12 +0500

    pushed

--- eat-easy-menu/sanity.cli.ts
+++ eat-easy-menu/sanity.cli.ts
@@ -3,13 +3,18 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'quztwy52',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    /**
+     * The appId from your deployed Studio.
+     * Get it after running `sanity deploy` or from sanity.io/manage
+     */
+    appId: 'kitpejzy2jkj4zpsnsyzt4qp',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+  },
 })
